0a9db9860fdde77259cfb39749a412c315406093
gowthampandiyank <gowthampandiyan7@gmail.com>
2025-12-02 21:37:21 +0530

    file modifide

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
 const toggleBtn = document.getElementById("theme_toggle");
-const icon = document.getElementById("theme_icon");
+const themeIcon = document.getElementById("theme_icon");
 
-// Load previous theme
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark-mode");
-  icon.className = "fa-solid fa-sun";
-}
-
-// Toggle theme
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 
-  const isDark = document.body.classList.contains("dark-mode");
-
-  if (isDark) {
-    localStorage.setItem("theme", "dark");
-    icon.className = "fa-solid fa-sun";
+  if (document.body.classList.contains("dark-mode")) {
+    themeIcon.classList.replace("fa-moon", "fa-sun");
   } else {
-    localStorage.removeItem("theme");
-    icon.className = "fa-solid fa-moon";
+    themeIcon.classList.replace("fa-sun", "fa-moon");
   }
 });
